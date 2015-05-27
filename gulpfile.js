1c074c395af3ae58785a25a645e9954983212fa3
zavier <zavier@debian>
2015-05-28 02:32:26 +0700

    Cleanups, cleanups, and more cleanups

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,5 @@ gulp.task('default', function () {
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(prefix('last 2 version', 'ie 8', 'ie 9'))
-    .pipe(gulp.dest('./output/main.css'));
+    .pipe(gulp.dest('./output/'));
 });
