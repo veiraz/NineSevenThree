ad3ab6c067e5bf8c76ab252fe8d40344f692718a
zavier <zavier@debian>
2015-05-31 14:28:29 +0700

    Push changes for the day

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp');
-var prefix = require('gulp-autoprefixer');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    prefix = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    util = require('gulp-util');
 
 gulp.task('default', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 version', 'ie 8', 'ie 9'))
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./output/'));
+    .pipe(gulp.dest('./output/'))
+    return util.log('Done! Now grab the framework on /output/main.css');
 });
